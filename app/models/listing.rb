e74980f6bcfe051cd7b24d5986483777863b7e38
Wenqian <wuwq85@gmail.com>
2014-06-22 01:10:24 -0700

    differentiate localhost and heroku

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,12 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no_image_available.jpeg",
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no_image_available.jpeg"
+	else	
+		has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no_image_available.jpeg",
 		    			:storage => :dropbox,
     					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+						:path=>":style/:id_:filename"
+	end
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
