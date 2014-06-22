41c9f7561b2fac14c23ceeb8ecb1a296a01186be
Wenqian <wuwq85@gmail.com>
2014-06-22 01:17:33 -0700

    bug fix cont.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
 		has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => "no_image_available.jpeg",
 		    			:storage => :dropbox,
     					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-						:path=>":style/:id_:filename"
+						:path => ":style/:id_:filename"
 	end
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
