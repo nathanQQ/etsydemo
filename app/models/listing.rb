c198109b7c9ae57d608572ef31e7affb0beaf673
Wenqian <wuwq85@gmail.com>
2014-07-01 02:26:55 -0700

    change user log form format, add validates

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,5 +8,7 @@ class Listing < ActiveRecord::Base
 						:path => ":style/:id_:filename"
 	end
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :name, :price, :description, presence: true
+	validates :price, numericality:{greater_than: 0}
 
 end
